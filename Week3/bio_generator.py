ade7fd8bafa7ff71479fd9a4a2def7ac201a2b01
Conner Monahan <Conner.Monahan@gmail.com>
2026-01-23 20:37:01 -0500

    MIDPOINT

---
+++ Week3/bio_generator.py
@@ -0,0 +1,17 @@
+# Import Statements
+
+# Function Definitions
+
+# Program Logic
+# Assign values to variables: name, goal, birth_year, current_year
+name = "Conner Monahan"
+goal = "to be the very best coder"
+birth_year = 1998
+current_year = 2026
+# Calculate Age
+age = current_year - birth_year
+# Print top border
+print ("=" * 25)
+# Print bio sentence
+print ("My name is " + name + " and I am " + str(age) + " years old.")
+# Print bottom border
