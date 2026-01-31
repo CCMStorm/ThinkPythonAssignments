60d1b3a5966b5b6dbf7d023e1e9418d6b4e56472
Conner Monahan <Conner.Monahan@gmail.com>
2026-01-30 22:48:33 -0500

    COMPLETION

--- Week 4/print_bio.py
+++ Week 4/print_bio.py
@@ -3,6 +3,8 @@
 
 # FUNCTION DEFINITIONS
 # 1. Define print_bio with three parameters: name, age, goal
+# 2. Inside the function, print a border line using string multiplication (*)
+# 3. Print the bio details using the parameter names
 def print_bio(name, age, goal):
     # Print top border
     print("*" * 25)
@@ -11,14 +13,10 @@ def print_bio(name, age, goal):
     # Print bottom border
     print("*" * 25)
 
-# Test Call
-print_bio ( "Conner Monahan", 28, "Learn Python." )
-
-# 2. Inside the function, print a border line using string multiplication (*)
-
-# 3. Print the bio details using the parameter names
-
 # PROGRAM LOGIC
 # 1. Call print_bio using my own info
+print_bio ( "Conner Monahan", 28, "Learn Python." )
 # 2. Call print_bio using a famous person
+print_bio ( "Albert Einstein", 76, "Discover the theory of relativity." )
 # 3. Call print_bio using a fictional character
+print_bio ( "Harry Potter", 17, "Defeat Voldemort." )
