16e4b4392787a5eb773f468a03f9c413dcae98b2
Conner Monahan <Conner.Monahan@gmail.com>
2026-01-30 22:42:06 -0500

    MIDPOINT

--- Week 4/print_bio.py
+++ Week 4/print_bio.py
@@ -2,11 +2,23 @@
 # (None)
 
 # FUNCTION DEFINITIONS
-# 1. Define print_bio with three parameters: name, age, and goal.
-# 2. Inside the function, use string multiplication (*) for a border line.
-# 3. Print the bio details using the parameter names.
+# 1. Define print_bio with three parameters: name, age, goal
+def print_bio(name, age, goal):
+    # Print top border
+    print("*" * 25)
+    # Print bio sentence
+    print("Name:", name + ",", "Age:", age, "Goal:", goal)
+    # Print bottom border
+    print("*" * 25)
+
+# Test Call
+print_bio ( "Conner Monahan", 28, "Learn Python." )
+
+# 2. Inside the function, print a border line using string multiplication (*)
+
+# 3. Print the bio details using the parameter names
 
 # PROGRAM LOGIC
-# 1. Call print_bio using my own info.
-# 2. Call print_bio using [Name of Famous Person].
-# 3. Call print_bio using [Name of Fictional Character].
+# 1. Call print_bio using my own info
+# 2. Call print_bio using a famous person
+# 3. Call print_bio using a fictional character
