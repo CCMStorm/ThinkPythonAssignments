1e650d55086739dfec919e2b1c06a2c592a3e385
Conner Monahan <Conner.Monahan@gmail.com>
2026-02-13 12:15:31 -0500

    STRUCTURE & PSEUDOCODE

---
+++ Week 6/Chapter6.py
@@ -0,0 +1,15 @@
+# IMPORT STATEMENTS
+# 1. Import math
+
+# FUNCTION DEFINITIONS
+# 1. Define area(radius): returns pi * r squared
+# 2. Define circumference(radius): returns 2 * pi * r
+# 3. Define volume(radius, height): 
+#    - Call area(radius) and multiply by height.
+#    - Return the result.
+
+# PROGRAM LOGIC
+# 1. Input for radius (float)
+# 2. Call and print area and circumference.
+# 3. Input for height (float)
+# 4. Call and print volume.
