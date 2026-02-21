279fb6e60353e3aafd86fe176d381b702952c2fc
Conner Monahan <Conner.Monahan@gmail.com>
2026-02-21 13:31:31 -0500

    STRUCTURE & PSEUDOCODE

---
+++ Week 7/The Modular ATM System.py
@@ -0,0 +1,17 @@
+# IMPORT STATEMENTS
+# (None)
+
+# FUNCTION DEFINITIONS
+# 1. Define verify_pin(): 
+#    - Create a loop that asks for a PIN until it equals "1234"
+#    - Return True
+# 2. Define withdraw(balance, amount):
+#    - Check if enough money exists
+#    - Return the new (or old) balance
+
+# PROGRAM LOGIC
+# 1. Set current_balance to 500
+# 2. Call verify_pin()
+# 3. Get withdrawal amount from user input
+# 4. Update current_balance by calling withdraw()
+# 5. Print the final balance
