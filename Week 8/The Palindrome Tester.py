9f6a036891bc74e172d77f38f579f08b2a56479c
Conner Monahan <Conner.Monahan@gmail.com>
2026-02-27 23:29:07 -0500

    STRUCTURE & PSEUDOCODE

---
+++ Week 8/The Palindrome Tester.py
@@ -0,0 +1,15 @@
+# IMPORT STATEMENTS
+# No imports needed
+
+# FUNCTION DEFINITIONS
+# Define is_palindrome(word)
+#   Make the word lowercase 
+#   Reverse the word using slicing 
+#   Compare the lowercase word to the reversed word
+#   Return True if they match, otherwise return False
+
+# PROGRAM LOGIC
+# Ask the user for a word
+# Call is_palindrome on the user's word
+# If True, print "(word) is a palindrome"
+# Else, print "(word) is not a palindrome"
