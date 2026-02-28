6bfd0392bfed17b174af60c5ffd64ff30343f5df
Conner Monahan <Conner.Monahan@gmail.com>
2026-02-27 23:34:22 -0500

    COMPLETION

--- Week 8/The Palindrome Tester.py
+++ Week 8/The Palindrome Tester.py
@@ -7,9 +7,23 @@
 #   Reverse the word using slicing 
 #   Compare the lowercase word to the reversed word
 #   Return True if they match, otherwise return False
+def is_palindrome(word):
+    word = word.lower()
+    reversed_word = word[::-1]
+    
+    if word == reversed_word:
+        return True
+    else:
+        return False
 
 # PROGRAM LOGIC
 # Ask the user for a word
+word = input("Enter a word: ")
 # Call is_palindrome on the user's word
+result = is_palindrome(word)
 # If True, print "(word) is a palindrome"
 # Else, print "(word) is not a palindrome"
+if result:
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
